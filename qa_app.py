1d2155800a2dad04b728014e0a0e97b4937c1bbc
exploevo <115216906+ZiggyReQurv@users.noreply.github.com>
2023-07-20 13:25:00 +0200

    First Commit
    Small Chaged

--- qa_app.py
+++ qa_app.py
@@ -16,7 +16,8 @@ from langchain.callbacks.base import CallbackManager
 from langchain.embeddings import HuggingFaceEmbeddings
 
 
-st.set_page_config(page_title="PDF Analyzer",page_icon=':shark:')
+st.set_page_config(page_title="PDF Analyzer", page_icon=":shark:")
+
 
 @st.cache_data
 def load_docs(files):
@@ -35,10 +36,8 @@ def load_docs(files):
             text = stringio.read()
             all_text += text
         else:
-            st.warning('Please provide txt or pdf.', icon="⚠️")
+            st.warning("Please provide txt or pdf.", icon="⚠️")
     return all_text
-
-
 
 
 @st.cache_resource
@@ -55,9 +54,9 @@ def create_retriever(_embeddings, splits, retriever_type):
 
     return retriever
 
+
 @st.cache_resource
 def split_texts(text, chunk_size, overlap, split_method):
-
     # Split texts
     # IN: text, chunk size, overlap, split_method
     # OUT: list of str splits
@@ -66,7 +65,8 @@ def split_texts(text, chunk_size, overlap, split_method):
 
     split_method = "RecursiveTextSplitter"
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=overlap)
+        chunk_size=chunk_size, chunk_overlap=overlap
+    )
 
     splits = text_splitter.split_text(text)
     if not splits:
@@ -75,34 +75,34 @@ def split_texts(text, chunk_size, overlap, split_method):
 
     return splits
 
+
 @st.cache_data
 def generate_eval(text, N, chunk):
-
     # Generate N questions from context of chunk chars
     # IN: text, N questions, chunk size to draw question from in the doc
     # OUT: eval set as JSON list
 
     st.info("`Generating sample questions ...`")
     n = len(text)
-    starting_indices = [random.randint(0, n-chunk) for _ in range(N)]
-    sub_sequences = [text[i:i+chunk] for i in starting_indices]
+    starting_indices = [random.randint(0, n - chunk) for _ in range(N)]
+    sub_sequences = [text[i : i + chunk] for i in starting_indices]
     chain = QAGenerationChain.from_llm(ChatOpenAI(temperature=0))
     eval_set = []
     for i, b in enumerate(sub_sequences):
         try:
             qa = chain.run(b)
             eval_set.append(qa)
-            st.write("Creating Question:",i+1)
+            st.write("Creating Question:", i + 1)
         except:
-            st.warning('Error generating question %s.' % str(i+1), icon="⚠️")
+            st.warning("Error generating question %s." % str(i + 1), icon="⚠️")
     eval_set_full = list(itertools.chain.from_iterable(eval_set))
     return eval_set_full
 
 
 # ...
 
+
 def main():
-    
     foot = f"""
     <div style="
         position: fixed;
@@ -113,17 +113,17 @@ def main():
         padding: 0px 0px;
         text-align: center;
     ">
-        <p>Made by <a href='https://twitter.com/mehmet_ba7'>Mehmet Balioglu</a></p>
+
     </div>
     """
 
     st.markdown(foot, unsafe_allow_html=True)
-    
+
     # Add custom CSS
     st.markdown(
         """
         <style>
-        
+
         #MainMenu {visibility: hidden;
         # }
             footer {visibility: hidden;
@@ -136,7 +136,7 @@ def main():
                 margin-bottom: 10px;
                 font-family: "IBM Plex Sans", sans-serif;
             }
-            
+
             .card-tag {
                 border-radius: 0px;
                 padding: 1px 5px 1px 5px;
@@ -149,95 +149,96 @@ def main():
                 color: white;
                 background-color: green;
                 }
-                
+
             .css-zt5igj {left:0;
             }
-            
+
             span.css-10trblm {margin-left:0;
             }
-            
+
             div.css-1kyxreq {margin-top: -40px;
             }
-            
-           
-       
-            
-          
+
+
+
+
+
 
         </style>
         """,
         unsafe_allow_html=True,
     )
-    st.sidebar.image("img/logo1.png")
-
-
-   
+    # st.sidebar.image("img/logo1.png")
 
     st.write(
-    f"""
+        f"""
     <div style="display: flex; align-items: center; margin-left: 0;">
         <h1 style="display: inline-block;">PDF Analyzer</h1>
         <sup style="margin-left:5px;font-size:small; color: green;">beta</sup>
     </div>
     """,
-    unsafe_allow_html=True,
-        )
-    
-    
+        unsafe_allow_html=True,
+    )
 
-
-    
-    
     st.sidebar.title("Menu")
-    
-    embedding_option = st.sidebar.radio(
-        "Choose Embeddings", ["OpenAI Embeddings", "HuggingFace Embeddings(slower)"])
 
-    
+    embedding_option = st.sidebar.radio(
+        "Choose Embeddings", ["OpenAI Embeddings", "HuggingFace Embeddings(slower)"]
+    )
+
     retriever_type = st.sidebar.selectbox(
-        "Choose Retriever", ["SIMILARITY SEARCH", "SUPPORT VECTOR MACHINES"])
+        "Choose Retriever", ["SIMILARITY SEARCH", "SUPPORT VECTOR MACHINES"]
+    )
 
     # Use RecursiveCharacterTextSplitter as the default and only text splitter
     splitter_type = "RecursiveCharacterTextSplitter"
 
-    if 'openai_api_key' not in st.session_state:
+    if "openai_api_key" not in st.session_state:
         openai_api_key = st.text_input(
-            'Please enter your OpenAI API key or [get one here](https://platform.openai.com/account/api-keys)', value="", placeholder="Enter the OpenAI API key which begins with sk-")
+            "Please enter your OpenAI API key or [get one here](https://platform.openai.com/account/api-keys)",
+            value="",
+            placeholder="Enter the OpenAI API key which begins with sk-",
+        )
         if openai_api_key:
             st.session_state.openai_api_key = openai_api_key
             os.environ["OPENAI_API_KEY"] = openai_api_key
         else:
-            #warning_text = 'Please enter your OpenAI API key. Get yours from here: [link](https://platform.openai.com/account/api-keys)'
-            #warning_html = f'<span>{warning_text}</span>'
-            #st.markdown(warning_html, unsafe_allow_html=True)
+            # warning_text = 'Please enter your OpenAI API key. Get yours from here: [link](https://platform.openai.com/account/api-keys)'
+            # warning_html = f'<span>{warning_text}</span>'
+            # st.markdown(warning_html, unsafe_allow_html=True)
             return
     else:
         os.environ["OPENAI_API_KEY"] = st.session_state.openai_api_key
 
-    uploaded_files = st.file_uploader("Upload a PDF or TXT Document", type=[
-                                      "pdf", "txt"], accept_multiple_files=True)
+    uploaded_files = st.file_uploader(
+        "Upload a PDF or TXT Document", type=["pdf", "txt"], accept_multiple_files=True
+    )
 
     if uploaded_files:
         # Check if last_uploaded_files is not in session_state or if uploaded_files are different from last_uploaded_files
-        if 'last_uploaded_files' not in st.session_state or st.session_state.last_uploaded_files != uploaded_files:
+        if (
+            "last_uploaded_files" not in st.session_state
+            or st.session_state.last_uploaded_files != uploaded_files
+        ):
             st.session_state.last_uploaded_files = uploaded_files
-            if 'eval_set' in st.session_state:
-                del st.session_state['eval_set']
+            if "eval_set" in st.session_state:
+                del st.session_state["eval_set"]
 
         # Load and process the uploaded PDF or TXT files.
         loaded_text = load_docs(uploaded_files)
         st.write("Documents uploaded and processed.")
 
         # Split the document into chunks
-        splits = split_texts(loaded_text, chunk_size=1000,
-                             overlap=0, split_method=splitter_type)
+        splits = split_texts(
+            loaded_text, chunk_size=1000, overlap=0, split_method=splitter_type
+        )
 
         # Display the number of text chunks
         num_chunks = len(splits)
         st.write(f"Number of text chunks: {num_chunks}")
 
         # Embed using OpenAI embeddings
-            # Embed using OpenAI embeddings or HuggingFace embeddings
+        # Embed using OpenAI embeddings or HuggingFace embeddings
         if embedding_option == "OpenAI Embeddings":
             embeddings = OpenAIEmbeddings()
         elif embedding_option == "HuggingFace Embeddings(slower)":
@@ -246,23 +247,29 @@ def main():
 
         retriever = create_retriever(embeddings, splits, retriever_type)
 
-
         # Initialize the RetrievalQA chain with streaming output
         callback_handler = StreamingStdOutCallbackHandler()
         callback_manager = CallbackManager([callback_handler])
 
         chat_openai = ChatOpenAI(
-            streaming=True, callback_manager=callback_manager, verbose=True, temperature=0)
-        qa = RetrievalQA.from_chain_type(llm=chat_openai, retriever=retriever, chain_type="stuff", verbose=True)
+            streaming=True,
+            callback_manager=callback_manager,
+            verbose=True,
+            temperature=0,
+        )
+        qa = RetrievalQA.from_chain_type(
+            llm=chat_openai, retriever=retriever, chain_type="stuff", verbose=True
+        )
 
         # Check if there are no generated question-answer pairs in the session state
-        if 'eval_set' not in st.session_state:
+        if "eval_set" not in st.session_state:
             # Use the generate_eval function to generate question-answer pairs
             num_eval_questions = 10  # Number of question-answer pairs to generate
             st.session_state.eval_set = generate_eval(
-                loaded_text, num_eval_questions, 3000)
+                loaded_text, num_eval_questions, 3000
+            )
 
-       # Display the question-answer pairs in the sidebar with smaller text
+        # Display the question-answer pairs in the sidebar with smaller text
         for i, qa_pair in enumerate(st.session_state.eval_set):
             st.sidebar.markdown(
                 f"""
